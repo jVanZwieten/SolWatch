9ffc552c28a205db632fce12c049e7d4bd2e90ab
John VanZwieten <john.vanzwieten@outlook.com>
2024-07-21 22:11:34 -0600

    Implement Planet.Anomaly(). Add unit  test project. Test Planet.Anomaly().

---
+++ SolWatch/EpochAnomaly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SolWatch
+{
+    public readonly struct EpochAnomaly
+    {
+        public DateTime Epoch { get; }
+        public float TrueAnomaly { get; } // radians
+
+        public EpochAnomaly(DateTime epoch, float trueAnomaly)
+        {
+            Epoch = epoch;
+            TrueAnomaly = trueAnomaly;
+        }
+    }
+}
--- SolWatch/Planet.cs
+++ SolWatch/Planet.cs
@@ -9,15 +9,19 @@ namespace SolWatch
         public float SemiMajorAxis { get; } // km
         public float LongitudeOfAscendingNode { get; } // radians
         public float ArgumentOfPeriapsis { get; } // radians
-        public List<SolarPosition> Anomalies { get; }
 
-        public Planet(string name, float semiMajorAxis, float longitudeOfAscendingNode, float argumentOfPeriapsis)
+        readonly EpochAnomaly referencePosition;
+
+        float meanMotion { get => MathF.Sqrt(Utilities.GravitationalParameter_Sol / MathF.Pow(SemiMajorAxis, 3)); } // radians/s
+        float period { get => Utilities.FullCircle / meanMotion; } // s
+
+        public Planet(string name, float semiMajorAxis, float longitudeOfAscendingNode, float argumentOfPeriapsis, EpochAnomaly referencePosition)
         {
             Name = name;
             SemiMajorAxis = semiMajorAxis;
             LongitudeOfAscendingNode = longitudeOfAscendingNode;
             ArgumentOfPeriapsis = argumentOfPeriapsis;
-            Anomalies = new List<SolarPosition>();
+            this.referencePosition = referencePosition;
         }
 
         /// <summary>
@@ -25,9 +29,6 @@ namespace SolWatch
         /// </summary>
         /// <param name="epoch">The moment at which you want the planet's position.</param>
         /// <returns>Mean/true anomaly (approximating a circular orbit) of the planet at the requested epoch. Angle through its orbit, in radians.</returns>
-        public float Anomaly(DateTime epoch)
-        {
-            throw new NotImplementedException();
-        }
+        public float Anomaly(DateTime epoch) => Utilities.NormalizeAngle(referencePosition.TrueAnomaly + (meanMotion * (float)(epoch - referencePosition.Epoch).TotalSeconds));
     }
 }
